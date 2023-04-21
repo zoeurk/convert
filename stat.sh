891b490876fbb1bd758c0fe29f306241a4b8ce77
zoeurk <zoeurk@gmail.com>
2023-04-21 22:55:07 +0200

    update

--- stat.sh
+++ stat.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+C="%"
 if test -z "$1"
 then
 	echo "Usage: $0 repertoire"
@@ -6,6 +7,7 @@ then
 fi
 DIR="$(mktemp -d)"
 TRACK=`find "$1" -name \*.ogg -print | cat -n | tail -n 1 | sed 's/ *\([0-9]\+\).*/\1/'`
+TOTGRP=$(ls -1 "$1" | cat -n |  tail -n 1 | sed 's/ *\([0-9]\+\).*/\1/')
 ls -1 "$1" | \
 while read E;
 do
@@ -14,8 +16,9 @@ done >> "$DIR"/grp.txt
 while read GRP
 do
 	G_TRACK="$(find "$1"/"$GRP" -name \*.ogg -print | cat -n | tail -n 1 | sed 's/ *\([0-9]\+\).*/\1/')"
-	POURCENTAGE="$(echo "scale=6;100*$G_TRACK/$TRACK" | bc)"
+	POURCENTAGE="$(printf "scale=6;100*$G_TRACK/$TRACK\n" | bc)"
 	test -z ${POURCENTAGE%%.*} && POURCENTAGE=0$POURCENTAGE
 	echo "$GRP: $POURCENTAGE%"
 done < "$DIR"/grp.txt
+printf "Groupe = $TOTGRP Titre: $TRACK\n"
 rm -r $DIR
